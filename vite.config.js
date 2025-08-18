5f666f4784335f4583d12a1c9f362bb5e37ff5b3
fadeyibi gbolahan <fadeyibi26@gmail.com>
2025-08-18 04:59:30 +0100

    domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/trustmart_frontend/", // repo name with slashes
+  base: "/", // repo name with slashes
   plugins: [react()],
   resolve: {
     alias: {
