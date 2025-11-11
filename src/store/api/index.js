849e22d345f2563bb67389e9415ca21ef3227939
fadeyibi gbolahan <fadeyibi26@gmail.com>
2025-11-11 09:48:35 +0100

    url

--- src/store/api/index.js
+++ src/store/api/index.js
@@ -1,2 +1,3 @@
 // export const url = "http://localhost:5000/api/";
-export const url = "https://trustmart-backend.onrender.com/api/";
+// export const url = "https://trustmart-backend.onrender.com/api/";
+export const url = "https://api.trustmart.com.ng/api/";
